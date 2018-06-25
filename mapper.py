01d38ef2af208c4a3bda13f54f137cd0f3453b00
Anubhav Singh <35691558+anubhavs11@users.noreply.github.com>
2018-06-25 15:50:45 +0530

    Mappers Job to find most popular programming lang.

---
+++ mapper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import json
+import sys
+import re
+
+ 
+tweets_data = []
+tweets_file = sys.stdin
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+
+for text in tweets_data:
+	
+	tweet = text['text'].lower()
+	
+	if(re.search("java",tweet)):
+		print("java",1)
+
+	if(re.search("python",tweet)):
+		print("python",1)
+
+	if(re.search("cpp",tweet)):
+		print("cpp",1)
